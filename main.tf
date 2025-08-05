2b751f0128aee6fadb6b591d48d457705457c71b
AKTECHLEARN <AKTECHLEARN@GMAIL.COM>
2025-08-05 12:16:32 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = ">= 1.7.0"
+  required_version = ">= 1.6.0"
 
   required_providers {
     azurerm = {
@@ -11,44 +11,27 @@ terraform {
 
 provider "azurerm" {
   features = {}
+
   client_id       = var.client_id
   client_secret   = var.client_secret
   tenant_id       = var.tenant_id
   subscription_id = var.subscription_id
 }
 
-# Authentication via GitHub Secrets
 variable "client_id" {}
 variable "client_secret" {}
 variable "tenant_id" {}
 variable "subscription_id" {}
 
-# Folder where ZIP will be extracted
-variable "unzipped_path" {
-  default = "unzipped"
+resource "azurerm_resource_group" "example" {
+  name     = "rg-example"
+  location = "East US"
 }
 
-# Existing Storage Account and Container
-variable "existing_storage_account_name" {
-  default = "kusaltest"
-}
-
-variable "existing_container_name" {
-  default = "mycontainer"
-}
-
-# Get list of files in unzipped folder
-locals {
-  files = fileset(var.unzipped_path, "**/*")
-}
-
-# Upload all files to the blob container
-resource "azurerm_storage_blob" "uploads" {
-  for_each = { for file in local.files : file => file }
-
-  name                   = each.key
-  source                 = "${var.unzipped_path}/${each.value}"
-  storage_account_name   = var.existing_storage_account_name
-  storage_container_name = var.existing_container_name
-  type                   = "Block"
+resource "azurerm_storage_account" "example" {
+  name                     = "examplestoracct123"   # Must be globally unique
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
 }
